5d57ce7a962ca6aa869c0667a46ddb322de663b2
Anthony <acevedo.cardenas.anthony@gmail.com>
2026-03-21 01:34:37 -0600

    Comentarios de cada parte del codigo
    Explica cada parte del codigo

--- Practica2_BusquedaInformada/Puzzle_8&15_A/modelo.py
+++ Practica2_BusquedaInformada/Puzzle_8&15_A/modelo.py
@@ -3,17 +3,27 @@ import numpy as np
 class PuzzleModel:
     @staticmethod
     def get_pos(val, goal):
+        """Busca las coordenadas (fila, columna) de un valor en la matriz meta."""
         res = np.where(goal == val)
         return res[0][0], res[1][0]
 
     @staticmethod
     def h_misplaced(state, goal):
-        # Fichas fuera de lugar (ignora el cero)
+        """
+        Heurística de Fichas Fuera de Lugar:
+        Cuenta cuántas piezas no están en su posición correcta.
+        Es una estimación simple pero menos precisa que Manhattan.
+        """
         return np.sum((state != goal) & (state != 0))
 
     @staticmethod
     def h_manhattan(state, goal):
-        # Distancia Manhattan pura
+        """
+        Heurística de Distancia Manhattan:
+        Calcula la suma de las distancias horizontales y verticales que cada ficha 
+        debe recorrer para llegar a su posición objetivo.
+        Es mucho más eficiente para guiar al algoritmo A*.
+        """
         dist = 0
         for i in range(state.shape[0]):
             for j in range(state.shape[1]):
